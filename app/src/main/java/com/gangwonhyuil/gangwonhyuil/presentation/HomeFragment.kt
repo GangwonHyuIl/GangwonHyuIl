5f97c9ed1a103f4f94e7d20019562d22a829e52e
wade316 <sdc1565@gmail.com>
2024-08-02 15:50:10 +0900

    홈화면 바텀네비게이션 프레임 생성

---
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/presentation/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.gangwonhyuil.gangwonhyuil.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gangwonhyuil.gangwonhyuil.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

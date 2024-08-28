830890fbb235826080f4836897887577e7f5c86c
sunjoolee <sheisthepoem@naver.com>
2024-08-29 01:53:36 +0900

    Feat: Inherit BaseFragment

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/community/screen/community/CommunityFragment.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/community/screen/community/CommunityFragment.kt
@@ -9,40 +9,43 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.gangwonhyuil.gangwonhyuil.R
+import com.gangwonhyuil.gangwonhyuil.databinding.FragmentCommunityBinding
+import com.gangwonhyuil.gangwonhyuil.util.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-interface OnPostItemClickListener{
+interface OnPostItemClickListener {
     fun onPostItemClick(id: Long)
 }
 
 @AndroidEntryPoint
-class CommunityFragment : Fragment(), OnPostItemClickListener {
+class CommunityFragment : BaseFragment<FragmentCommunityBinding>(), OnPostItemClickListener {
     private val viewModel by lazy {
         ViewModelProvider(this@CommunityFragment)[CommunityViewModel::class.java]
     }
 
     private lateinit var placeItemAdapter: PlaceItemAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun inflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentCommunityBinding {
+        return FragmentCommunityBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         initAdapter()
         initViewModelObserver()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_community, container, false)
-    }
-
-    private fun initAdapter(){
+    private fun initAdapter() {
         placeItemAdapter = PlaceItemAdapter(this@CommunityFragment)
     }
 
-    private fun initViewModelObserver(){
+    private fun initViewModelObserver() {
         lifecycleScope.launch {
             viewModel.postItems.collectLatest { postItems ->
                 placeItemAdapter.updateItems(postItems)

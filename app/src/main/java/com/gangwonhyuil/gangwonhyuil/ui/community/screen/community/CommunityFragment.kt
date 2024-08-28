32681b5ea46bfd2e67a007a2074de0bff15e527f
sunjoolee <sheisthepoem@naver.com>
2024-08-29 02:05:58 +0900

    Feat: Top app bar menu click listener

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/community/screen/community/CommunityFragment.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/community/screen/community/CommunityFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.gangwonhyuil.gangwonhyuil.R
 import com.gangwonhyuil.gangwonhyuil.databinding.FragmentCommunityBinding
 import com.gangwonhyuil.gangwonhyuil.util.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,8 +36,24 @@ class CommunityFragment : BaseFragment<FragmentCommunityBinding>(), OnPostItemCl
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initTopAppBar()
         initPlaceItemRecyclerView()
         initViewModelObserver()
+    }
+
+    private fun initTopAppBar() {
+        with(binding.topAppBar) {
+            setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.add_post -> {
+                        // TODO: start add post activity
+                        Timber.d("add post menu clicked")
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
     }
 
     private fun initPlaceItemRecyclerView() {
@@ -54,7 +70,7 @@ class CommunityFragment : BaseFragment<FragmentCommunityBinding>(), OnPostItemCl
     }
 
     override fun onPostItemClick(id: Long) {
-        // TODO: start post detail activity
+        // TODO: start post detail activity, send post id
         Timber.d("post item clicked) id: ${id}")
     }
 }

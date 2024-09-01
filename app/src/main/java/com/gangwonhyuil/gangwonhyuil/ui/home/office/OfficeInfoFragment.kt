c91fa352a1da1efeab5504f29d4b1901cdbf1c12
sunjoolee <sheisthepoem@naver.com>
2024-09-02 01:28:55 +0900

    Style: Reformat code

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/home/office/OfficeInfoFragment.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/home/office/OfficeInfoFragment.kt
@@ -12,25 +12,28 @@ import com.gangwonhyuil.gangwonhyuil.util.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import timber.log.Timber
+
 @AndroidEntryPoint
 class OfficeInfoFragment : BaseFragment<FragmentOfficeInfoBinding>() {
     private val viewModel: OfficeViewModel by viewModels()
     private lateinit var officeAdapter: OfficeAdapter
+
     override fun inflateBinding(
         inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentOfficeInfoBinding {
-        return FragmentOfficeInfoBinding.inflate(inflater, container, false)
-    }
+        container: ViewGroup?,
+    ): FragmentOfficeInfoBinding = FragmentOfficeInfoBinding.inflate(inflater, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
         super.onViewCreated(view, savedInstanceState)
 
         setCategoryTitle()
         initRecyclerView()
         observeViewModel()
-
     }
+
     private fun setCategoryTitle() {
         val category = arguments?.getString(ARG_CATEGORY) ?: "카테고리 없음"
         Timber.d(category)

1577628fda41cd30b596beacf737b8ea28663f0c
sunjoolee <sheisthepoem@naver.com>
2024-09-02 01:45:55 +0900

    Fix: Fix imports

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/home/office/OfficeViewModel.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/home/office/OfficeViewModel.kt
@@ -1,6 +1,6 @@
 package com.gangwonhyuil.gangwonhyuil.ui.home.office
 
-import com.gangwonhyuil.gangwonhyuil.ui.BaseViewModel
+import com.gangwonhyuil.gangwonhyuil.util.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow

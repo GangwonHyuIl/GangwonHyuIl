dcb34b351902bc9479b9c0d0df1bad83b10452cc
nhkim <nhkim1529@gmail.com>
2024-09-02 23:36:01 +0900

    Feat: 홈 화면 리사이클러뷰 항목 Item UI 수정

---
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/ui/home/detail/DetailItemAdapter.kt
@@ -0,0 +1,40 @@
+package com.gangwonhyuil.gangwonhyuil.ui.home.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import coil.load
+import com.gangwonhyuil.gangwonhyuil.databinding.RvItemOfficeBinding
+import com.gangwonhyuil.gangwonhyuil.ui.home.office.Office
+import com.gangwonhyuil.gangwonhyuil.util.base.BaseAdapter
+
+class DetailItemAdapter() : BaseAdapter<Office>() {
+    override fun createBinding(parent: ViewGroup, viewType: Int): ViewBinding {
+        return RvItemOfficeBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+    }
+
+    override fun createViewHolder(binding: ViewBinding): BaseViewHolder<Office> {
+        return OfficeItemHolder(binding as RvItemOfficeBinding)
+    }
+
+    inner class OfficeItemHolder(binding: RvItemOfficeBinding) :
+        BaseViewHolder<Office>(binding.root) {
+        private val officeImageView = binding.ivItemImage
+        private val officeNameTextView = binding.tvItemName
+        private val officeRatingTextView = binding.tvItemRating
+
+        override fun bind(item: Office) {
+            if (item is Office.OfficeItem) {
+                officeImageView.load(item.image) {
+                    crossfade(true)
+                }
+                officeNameTextView.text = item.name
+                officeRatingTextView.text = item.rating
+            }
+        }
+    }
+}

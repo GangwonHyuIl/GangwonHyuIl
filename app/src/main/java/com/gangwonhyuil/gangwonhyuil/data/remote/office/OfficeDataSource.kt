3c89d6bcb7e393b8e9705b536f0b82121239cb02
nhkim <nhkim1529@gmail.com>
2024-09-10 02:23:02 +0900

    Feat: 공유 오피스 API 인터페이스 추가

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/remote/office/OfficeDataSource.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/remote/office/OfficeDataSource.kt
@@ -1,4 +1,13 @@
 package com.gangwonhyuil.gangwonhyuil.data.remote.office
 
-class OfficeDataSource {
+import com.gangwonhyuil.gangwonhyuil.data.response.office.OfficeResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OfficeDataSource {
+    @GET("share_office_list")
+    suspend fun getOfficeList(
+        @Query("share_office_location") location: String = "eq.1"
+    ): Response<OfficeResponse>
 }

66361bbed93c7fefa2bd675b13674df314d97dae
nhkim <nhkim1529@gmail.com>
2024-09-11 02:03:36 +0900

    Feat: 오피스 별점 받아오는 officeRatingResponse 추가

---
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/response/officeRating/OfficeRatingResponse.kt
@@ -0,0 +1,7 @@
+package com.gangwonhyuil.gangwonhyuil.data.response.officeRating
+
+data class officeRatingResponse (
+    val area_idx: Int,
+    val total_reviews: Int,
+    val average_rating: Int
+)

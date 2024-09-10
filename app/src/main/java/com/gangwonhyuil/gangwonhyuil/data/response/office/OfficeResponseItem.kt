473508b1cf9ceb843981be55ac18526ca8b16971
nhkim <nhkim1529@gmail.com>
2024-09-11 00:32:49 +0900

    Style: 변수명 camelCase로 변경

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/response/office/OfficeResponseItem.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/response/office/OfficeResponseItem.kt
@@ -1,6 +1,9 @@
 package com.gangwonhyuil.gangwonhyuil.data.response.office
 
+import com.google.gson.annotations.SerializedName
+
 data class OfficeResponseItem(
     val offices: List<Office>,
-    val share_office_location: Int
+    @SerializedName("share_office_location")
+    val location: Int
 )

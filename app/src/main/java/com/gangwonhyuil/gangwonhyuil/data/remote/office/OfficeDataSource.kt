a7c91654a175c5a33e67ecafd3b68c0a9184a362
nhkim <nhkim1529@gmail.com>
2024-09-11 02:04:29 +0900

    Feat: OfficeDataSource에 오피스 별점 받아오는 getOfficeRatingList 함수 추가

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/remote/office/OfficeDataSource.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/remote/office/OfficeDataSource.kt
@@ -1,8 +1,12 @@
 package com.gangwonhyuil.gangwonhyuil.data.remote.office
 
+import com.gangwonhyuil.gangwonhyuil.data.interceptor.PostJsonRequest
 import com.gangwonhyuil.gangwonhyuil.data.response.office.OfficeResponse
+import com.gangwonhyuil.gangwonhyuil.data.response.officeRating.officeRatingResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface OfficeDataSource {
@@ -10,4 +14,10 @@ interface OfficeDataSource {
     suspend fun getOfficeList(
         @Query("share_office_location") location: String = "eq.1"
     ): Response<OfficeResponse>
+
+    @PostJsonRequest
+    @POST("rpc/get_area_reviews")
+    suspend fun getOfficeRatingList(
+        @Body idList: List<String>
+    ): Response<officeRatingResponse>
 }

243f86fda758221cf36f660b9305ff8f3c49b55f
nhkim <nhkim1529@gmail.com>
2024-09-11 02:32:14 +0900

    Style: 변수명 camelCase로 변경

--- app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/response/officeRating/OfficeRatingResponse.kt
+++ app/src/main/java/com/gangwonhyuil/gangwonhyuil/data/response/officeRating/OfficeRatingResponse.kt
@@ -1,7 +1,12 @@
 package com.gangwonhyuil.gangwonhyuil.data.response.officeRating
 
+import com.google.gson.annotations.SerializedName
+
 data class officeRatingResponse (
-    val area_idx: Int,
-    val total_reviews: Int,
-    val average_rating: Int
+    @SerializedName("area_idx")
+    val id: Int,
+    @SerializedName("total_reviews")
+    val numOfReviews: Int,
+    @SerializedName("average_rating")
+    val rating: Int
 )
